9446166fdec0fe63d2c9cd969df219e2f919f65c
Kim-Tri Huynh <huynhkimtri.fuhcm@gmail.com>
2024-09-21 00:29:54 +0700

    feat: just add the logout function

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,17 @@
+import { useDispatch } from "react-redux";
+import { logout } from "../actions/auth";
+
 const Home = () => {
+  const dispatch = useDispatch();
+
+  const handleLogout = () => {
+    dispatch(logout());
+  };
+
   return (
     <div>
       <h1>Home page</h1>
+      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 };
