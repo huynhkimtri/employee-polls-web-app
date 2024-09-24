bd40baff38d8dcd9d791a36b822b14878ad97e34
Kim-Tri Huynh <huynhkimtri.fuhcm@gmail.com>
2024-09-24 22:26:22 +0700

    fix: update database

--- src/utils/_DATA.js
+++ src/utils/_DATA.js
@@ -4,13 +4,8 @@ let users = {
     password: "admin",
     name: "Administrator",
     avatarURL: "https://api.dicebear.com/7.x/miniavs/svg?seed=1",
-    answers: {
-      "8xf0y6ziyjabvozdd253nd": "optionOne",
-      "6ni6ok3ym7mf1p33lnez": "optionOne",
-      am8ehyc8byjqgar0jgpub9: "optionTwo",
-      loxhs1bqm25b708cmbf3g: "optionTwo",
-    },
-    questions: ["8xf0y6ziyjabvozdd253nd", "am8ehyc8byjqgar0jgpub9"],
+    answers: {},
+    questions: [],
   },
   sarahedo: {
     id: "sarahedo",
@@ -19,7 +14,7 @@ let users = {
     avatarURL: "https://api.dicebear.com/7.x/miniavs/svg?seed=2",
     answers: {
       "8xf0y6ziyjabvozdd253nd": "optionOne",
-      "6ni6ok3ym7mf1p33lnez": "optionOne",
+      "6ni6ok3ym7mf1p33lnez": "optionTwo",
       am8ehyc8byjqgar0jgpub9: "optionTwo",
       loxhs1bqm25b708cmbf3g: "optionTwo",
     },
@@ -44,7 +39,7 @@ let users = {
     answers: {
       xj352vofupe1dqz9emx13r: "optionOne",
       vthrdm985a262al8qx3do: "optionTwo",
-      "6ni6ok3ym7mf1p33lnez": "optionOne",
+      "6ni6ok3ym7mf1p33lnez": "optionTwo",
     },
     questions: ["6ni6ok3ym7mf1p33lnez", "xj352vofupe1dqz9emx13r"],
   },
